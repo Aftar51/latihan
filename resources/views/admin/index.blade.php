ee13de3fb2f2598ccd5407a15cdc31e7b020676a
aftar51 <afftardarda@gmail.com>
2024-02-16 14:52:00 +0700

    commit ya guys

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,4 +6,22 @@
     <a href="{{ route('admin.create') }}" class="btn btn-primary">Create admin</a>
 </div>
 
+<div class="container text-center">
+    <h5 class="fw-bold">USERS</h5>
+    <table class="table">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Gender</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <th>Aftar</th>
+            <th>a**********@gmail.com</th>
+            <th>Male</th>
+        </tr>
+    </table>
+</div>
+
 @endsection
